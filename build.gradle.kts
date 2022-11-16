076efaccb3445d6f9331f963993d3bd759fced92
funkatronics <funkatronicsmail@gmail.com>
2022-11-16 09:41:11 -0700

    retry... again

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,17 +81,7 @@ android {
 
 System.getenv("GITHUB_REPOSITORY")?.let {
 
-    val publishedGroupId: String by project
-    val libraryName: String by project
-    val libraryDescription: String by project
-    val siteUrl: String by project
-    val gitUrl: String by project
-    val licenseName: String by project
-    val licenseUrl: String by project
-    val developerOrg: String by project
-    val developerName: String by project
-    val developerEmail: String by project
-    val developerId: String by project
+    val publishedGroupId = "com.metaplex"
 
     project.group = publishedGroupId
     project.version = libraryVersion
@@ -102,34 +92,6 @@ System.getenv("GITHUB_REPOSITORY")?.let {
             version = libraryVersion
 
             artifact(tasks["javadocJar"])
-
-            pom {
-                name.set(libraryName)
-                description.set(libraryDescription)
-                url.set(siteUrl)
-
-                licenses {
-                    license {
-                        name.set(licenseName)
-                        url.set(licenseUrl)
-                    }
-                }
-                developers {
-                    developer {
-                        id.set(developerId)
-                        name.set(developerName)
-                        email.set(developerEmail)
-                    }
-                }
-                organization {
-                    name.set(developerOrg)
-                }
-                scm {
-                    connection.set(gitUrl)
-                    developerConnection.set(gitUrl)
-                    url.set(siteUrl)
-                }
-            }
         }
 
         repositories {
